6a0ef26cc6d97d19e4a2053ff836716652a271ec
Faaiz Amir <faaizammir@gmail.com>
2024-03-20 13:28:42 +0500

    Footer Sec 1 & 2 completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,68 @@
+
+const Footer = () => {
+  return (
+    <div>
+      <section className='w-full bg-[url(../../public/millennial-group-young-businesspeople.jpg)] bg-cover bg-no-repeat bg-top pt-36 pb-20 relative bg-gray-500 opacity-75 bg-blend-multiply '>
+        <div className="bg-green-primary opacity-60 absolute top-0 left-0 min-w-full min-h-full bg-blend-multiply"></div>
+        <div className='text-white m-auto w-2/3 container relative grid grid-cols-2'>
+            <div className="xl:col-span-1 w-[88%] col-span-2">
+            <h1 className='md:text-4xl text-2xl font-bold mb-4'>Internships Every Month</h1>
+            <p className="md:text-xl text-sm font-light">Introducing Internee.pk, the ultimate platform designed to turbocharge the IT sector in Pakistan! We recognize the immense potential of talented individuals in the country and aim to bridge the gap between them and the thriving IT industry. Internee.pk offers a comprehensive range of virtual internship opportunities exclusively in the IT field.   </p>
+            </div>
+        </div>
+      </section>
+      <section className="bg-white w-full py-20">
+        <div className="container flex flex-wrap m-auto gap-32  justify-center text-xl leading-10 font-thin">
+            <div>
+                <ul>
+                    <li className="Logo mb-4 ">
+                    <a className="navbar flex order-1 items-center lg:justify-center" href="index.html">
+                        <img className="sm:w-16 w-14 mr-0" src="../../public/1st-logo.jpg" alt="Logo"></img>
+                        <div className="flex flex-col">
+                            <strong className="logo-text text-green-primary text-xl font-bold sm:text-3xl ">Internee<span className='text-black'>.pk</span></strong>
+                            <small className="logo-slogan font-bold sm:text-xs text-[8px] leading-3 text-purple-primary">VIRTUAL INTERNSHIP PLATFORM</small>
+                        </div>
+                    </a>
+                    </li>
+                    <li className="hover:text-purple-primary pl-4"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" className="bi bi-globe inline-block mr-2 " viewBox="0 0 16 16">
+                        <path d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m7.5-6.923c-.67.204-1.335.82-1.887 1.855A8 8 0 0 0 5.145 4H7.5zM4.09 4a9.3 9.3 0 0 1 .64-1.539 7 7 0 0 1 .597-.933A7.03 7.03 0 0 0 2.255 4zm-.582 3.5c.03-.877.138-1.718.312-2.5H1.674a7 7 0 0 0-.656 2.5zM4.847 5a12.5 12.5 0 0 0-.338 2.5H7.5V5zM8.5 5v2.5h2.99a12.5 12.5 0 0 0-.337-2.5zM4.51 8.5a12.5 12.5 0 0 0 .337 2.5H7.5V8.5zm3.99 0V11h2.653c.187-.765.306-1.608.338-2.5zM5.145 12q.208.58.468 1.068c.552 1.035 1.218 1.65 1.887 1.855V12zm.182 2.472a7 7 0 0 1-.597-.933A9.3 9.3 0 0 1 4.09 12H2.255a7 7 0 0 0 3.072 2.472M3.82 11a13.7 13.7 0 0 1-.312-2.5h-2.49c.062.89.291 1.733.656 2.5zm6.853 3.472A7 7 0 0 0 13.745 12H11.91a9.3 9.3 0 0 1-.64 1.539 7 7 0 0 1-.597.933M8.5 12v2.923c.67-.204 1.335-.82 1.887-1.855q.26-.487.468-1.068zm3.68-1h2.146c.365-.767.594-1.61.656-2.5h-2.49a13.7 13.7 0 0 1-.312 2.5m2.802-3.5a7 7 0 0 0-.656-2.5H12.18c.174.782.282 1.623.312 2.5zM11.27 2.461c.247.464.462.98.64 1.539h1.835a7 7 0 0 0-3.072-2.472c.218.284.418.598.597.933M10.855 4a8 8 0 0 0-.468-1.068C9.835 1.897 9.17 1.282 8.5 1.077V4z"/>
+                    </svg>www.internee.pk</li>
+                    <li className="hover:text-purple-primary pl-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" className="bi bi-telephone inline-block mr-2" viewBox="0 0 16 16">
+                        <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.6 17.6 0 0 0 4.168 6.608 17.6 17.6 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.68.68 0 0 0-.58-.122l-2.19.547a1.75 1.75 0 0 1-1.657-.459L5.482 8.062a1.75 1.75 0 0 1-.46-1.657l.548-2.19a.68.68 0 0 0-.122-.58zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                    </svg>+92 315 4879020</li>
+                    <li className="hover:text-purple-primary pl-4">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="green" className="bi bi-envelope inline-block mr-2" viewBox="0 0 16 16">
+                        <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+                    </svg>faaizammir@gmail.com</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li className="font-bold text-2xl text-black mb-4">Company</li>
+                    <li className="hover:text-purple-primary">Home</li>
+                    <li className="hover:text-purple-primary">About</li>
+                    <li className="hover:text-purple-primary">Internships</li>
+                    <li className="hover:text-purple-primary">Contact</li>
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    <li className="font-bold text-2xl text-black mb-4">Resources</li>
+                    <li className="hover:text-purple-primary">Discord Server</li>
+                    <li className="hover:text-purple-primary">Blog</li>
+                    <li className="hover:text-purple-primary">Podcast</li>
+                </ul>
+            </div>
+            <div>
+                <h1 className="text-black font-bold text-2xl mb-4">Get Updates about internship</h1>
+                <input type="text" name="" id="" placeholder="yourname@gmail.com" className="outline-green-primary pl-2 w-3/4 mr-2 rounded-xl"/>
+                <button className="rounded-full h-14 w-14 bg-green-primary hover:bg-purple-primary">Send</button>
+            </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Footer
